feedcbdc18d6808f5b889540d7f4d9253a07654b
sinbee0402 <osb3476@gmail.com>
2022-11-09 10:24:05 +0900

    feat : SplashActivity
    SplashActivity.kt (기능) 구현
    - login 상태에 따른 view 전환
    - 사용자가 현재 로그인된 상태라면 MainActivity를 띄우고, 로그인 되지 않은 상태라면 SignupActivity를 띄운다

--- V2Eco3s/app/src/main/java/com/example/v2_eco3s/SplashActivity.kt
+++ V2Eco3s/app/src/main/java/com/example/v2_eco3s/SplashActivity.kt
@@ -4,22 +4,40 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SplashActivity : AppCompatActivity() {
 
     private val SPLASH_TIME_OUT: Long = 3000
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler().postDelayed({
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
-        }, SPLASH_TIME_OUT)
+        auth = Firebase.auth
     }
 
     override fun onBackPressed() {
         super.onBackPressed()
+    }
+
+    public override fun onStart() {
+        super.onStart()
+
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            Handler().postDelayed({
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }, SPLASH_TIME_OUT)
+        } else {
+            Handler().postDelayed({
+                startActivity(Intent(this, SignupActivity::class.java))
+                finish()
+            }, SPLASH_TIME_OUT)
+        }
     }
 }
