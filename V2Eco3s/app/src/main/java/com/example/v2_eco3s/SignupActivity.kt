d5853d411a4f02f1288a055f701b48e29f9add95
sinbee0402 <osb3476@gmail.com>
2022-11-11 12:09:25 +0900

    feat : SignupActivity
    BottomSheetBehavior 슬라이드 모션

--- V2Eco3s/app/src/main/java/com/example/v2_eco3s/SignupActivity.kt
+++ V2Eco3s/app/src/main/java/com/example/v2_eco3s/SignupActivity.kt
@@ -2,21 +2,24 @@ package com.example.v2_eco3s
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
+import android.os.Handler
 import com.google.android.material.bottomsheet.BottomSheetBehavior
-import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class SignupActivity : AppCompatActivity() {
+
+    private val TIME_OUT: Long = 150
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
 
-        var behavior = BottomSheetBehavior.from(this.findViewById(R.id.bottomSheet))
-
+        val behavior = BottomSheetBehavior.from(this.findViewById(R.id.bottomSheet))
         behavior.state = BottomSheetBehavior.STATE_HIDDEN
-        //behavior.state = BottomSheetBehavior.STATE_COLLAPSED
+
+        Handler().postDelayed({
+            //레이아웃에 저장한 peekHeight만큼 보임
+            behavior.state = BottomSheetBehavior.STATE_COLLAPSED
+        }, TIME_OUT)
     }
 
 }
